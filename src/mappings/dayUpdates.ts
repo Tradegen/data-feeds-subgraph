3e0e04ddb20dac15691753cff6c9d9db74fb68c1
Xavier Negron <xaviernegron888@gmail.com>
2022-07-08 12:34:04 -0500

    Update dayUpdates.ts

--- src/mappings/dayUpdates.ts
+++ src/mappings/dayUpdates.ts
@@ -1,8 +1,9 @@
 import { BigInt, ethereum } from "@graphprotocol/graph-ts";
 import {
-  BotPerformanceDataFeed,
   TradingBotDayData,
   TradingBotHourData,
+  VTEDayData,
+  VTEHourData,
   TradegenDayData,
   Tradegen,
 } from "../types/schema";
@@ -24,7 +25,9 @@ export function updateTradegenDayData(event: ethereum.Event): TradegenDayData {
     tradegenDayData.date = dayStartTimestamp;
     tradegenDayData.botPerformanceDataFeedUpdateCount = 0;
     tradegenDayData.candlestickDataFeedUpdateCount = 0;
-    tradegenDayData.dataRequestCount = 0;
+    tradegenDayData.VTEDataFeedUpdateCount = 0;
+    tradegenDayData.botDataRequestCount = 0;
+    tradegenDayData.VTEDataRequestCount = 0;
     tradegenDayData.revenue = ZERO_BI;
   }
 
@@ -87,4 +90,60 @@ export function updateTradingBotHourData(event: ethereum.Event, isDataRequest: b
   tradingBotHourData.save();
 
   return tradingBotHourData as TradingBotHourData;
+}
+
+export function updateVTEDayData(event: ethereum.Event, isDataRequest: boolean, VTEPrice: BigInt, fee: BigInt): VTEDayData {
+  let timestamp = event.block.timestamp.toI32();
+  let dayID = timestamp / 86400;
+  let dayStartTimestamp = dayID * 86400;
+  let dayVTEID = event.address
+    .toHexString()
+    .concat("-")
+    .concat(BigInt.fromI32(dayID).toString());
+  let vteDayData = VTEDayData.load(dayVTEID);
+  if (vteDayData === null) {
+    vteDayData = new VTEDayData(dayVTEID);
+    vteDayData.date = dayStartTimestamp;
+    vteDayData.VTEDataFeed = event.address.toHexString();
+    vteDayData.numberOfUpdates = 0;
+    vteDayData.numberOfDataRequests = 0;
+    vteDayData.VTEPrice = ZERO_BI;
+    vteDayData.revenue = ZERO_BI;
+  }
+
+  vteDayData.VTEPrice = VTEPrice;
+  vteDayData.numberOfUpdates = isDataRequest ? vteDayData.numberOfUpdates : vteDayData.numberOfUpdates + 1;
+  vteDayData.numberOfDataRequests = isDataRequest ? vteDayData.numberOfDataRequests + 1 : vteDayData.numberOfDataRequests;
+  vteDayData.revenue = isDataRequest ? vteDayData.revenue.plus(BigInt.fromString(fee.toString())) : vteDayData.revenue;
+  vteDayData.save();
+
+  return vteDayData as VTEDayData;
+}
+
+export function updateVTEHourData(event: ethereum.Event, isDataRequest: boolean, VTEPrice: BigInt, fee: BigInt): VTEHourData {
+  let timestamp = event.block.timestamp.toI32();
+  let hourIndex = timestamp / 3600; // get unique hour within unix history
+  let hourStartUnix = hourIndex * 3600; // want the rounded effect
+  let hourVTEID = event.address
+    .toHexString()
+    .concat("-")
+    .concat(BigInt.fromI32(hourIndex).toString());
+  let vteHourData = VTEHourData.load(hourVTEID);
+  if (vteHourData === null) {
+    vteHourData = new VTEHourData(hourVTEID);
+    vteHourData.hourStartUnix = hourStartUnix;
+    vteHourData.VTEDataFeed = event.address.toHexString();
+    vteHourData.numberOfUpdates = 0;
+    vteHourData.numberOfDataRequests = 0;
+    vteHourData.VTEPrice = ZERO_BI;
+    vteHourData.revenue = ZERO_BI;
+  }
+
+  vteHourData.VTEPrice = VTEPrice;
+  vteHourData.numberOfUpdates = isDataRequest ? vteHourData.numberOfUpdates : vteHourData.numberOfUpdates + 1;
+  vteHourData.numberOfDataRequests = isDataRequest ? vteHourData.numberOfDataRequests + 1 : vteHourData.numberOfDataRequests;
+  vteHourData.revenue = isDataRequest ? vteHourData.revenue.plus(BigInt.fromString(fee.toString())) : vteHourData.revenue;
+  vteHourData.save();
+
+  return vteHourData as VTEHourData;
 }
